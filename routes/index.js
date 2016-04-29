0565db39e61aea1931a08099b4c9595c7fb72201
Max Stern <stern.maxwell@gmail.com>
2016-04-29 16:17:58 -0500

    New burroughs svg

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET homepage. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Beat Generation Quote Generator' });
+  res.render('index', { title: 'Beaitfy | A Beat Generation Quote Generator' });
 });
 
 /* GET ginsberg */
 router.get('/kerouac', function(req, res, next) {
-  res.render('kerouac', { title: 'Jack Kerouac Quotes' });
+  res.render('kerouac', { title: 'Jack Kerouac Quotes | Beatify' });
 });
 
 /* GET ginsberg */
 router.get('/ginsberg', function(req, res, next) {
-  res.render('ginsberg', { title: 'Allen Ginsberg Quotes' });
+  res.render('ginsberg', { title: 'Allen Ginsberg Quotes | Beatify' });
 });
 
 /* GET snyder */
 router.get('/snyder', function(req, res, next) {
-  res.render('snyder', { title: 'Gary Snyder Quotes' });
+  res.render('snyder', { title: 'Gary Snyder Quotes | Beatify' });
 });
 
 /* GET burroughs */
 router.get('/burroughs', function(req, res, next) {
-  res.render('burroughs', { title: 'William S. Quotes' });
+  res.render('burroughs', { title: 'William S. Burroughs Quotes | Beatify' });
 });
 
 module.exports = router;
